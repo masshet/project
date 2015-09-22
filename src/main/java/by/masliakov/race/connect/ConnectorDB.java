84c98e2b7bd8640b0bb0781bebe458e23e3eda1b
mrstark <lmasliakov@gmail.com>
2015-09-22 15:29:20 +0300

    tree

--- src/main/java/by/masliakov/race/connect/ConnectorDB.java
+++ src/main/java/by/masliakov/race/connect/ConnectorDB.java
@@ -1,5 +1,6 @@
 package by.masliakov.race.connect;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.*;
@@ -10,22 +11,16 @@ import java.util.Properties;
  */
 public class ConnectorDB {
 
-    static Properties prop;
-    static
-    {
-        prop = new Properties();
-        try {
-            prop.load(new FileInputStream("WEB-INF/db.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    private static final String DRIVER = "oracle.jdbc.OracleDriver";
+    private static final String CONNECTION = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String USER = "Hippodrome";
+    private static final String PASSWORD = "qriddickp1";
 
     public static Connection getConnection() throws Exception
     {
 
-        Class.forName(prop.getProperty("driver"));
-        Connection con = DriverManager.getConnection(prop.getProperty("url"), prop.getProperty("user"), prop.getProperty("password"));
+        Class.forName(DRIVER);
+        Connection con = DriverManager.getConnection(CONNECTION, USER, PASSWORD);
 
         return con;
     }
